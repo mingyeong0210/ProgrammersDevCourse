2ebd3fa7db05941ed04a77211d4b3c5edf79fd2a
mingyeong0210 <mica7150@naver.com>
2025-02-04 16:06:57 +0900

    feat: index, router, server 역할 분리

--- 3주차/Login/server.js
+++ 3주차/Login/server.js
@@ -1,8 +1,12 @@
 let http = require('http'); //node.js가 http프로토콜을 사용할 수 있도록 모듈로 만들어둔 것을 require을 통해 불러서 사용 
+let url = require('url'); 
 
-function start() {
+function start(route) {
     // 서버를 생성하고 실행시키는 부분 
     function onRequest(request, response) { // node.js가 알아서 넣어줄 것임
+        let pathname = url.parse(request.url).pathname; //parse : 문자열 캐치
+        route(pathname);
+
         response.writeHead(200, {'Content-Type' : 'text/html'});
         response.write('Hello Node.js');
         response.end();
