4bb95f840acdb8b5b2df11ad23f0dabe6ba55c98
mingyeong0210 <mica7150@naver.com>
2025-02-04 15:46:02 +0900

    feat: server.js 모듈화

--- 3주차/Login/server.js
+++ 3주차/Login/server.js
@@ -1,9 +1,14 @@
 let http = require('http'); //node.js가 http프로토콜을 사용할 수 있도록 모듈로 만들어둔 것을 require을 통해 불러서 사용 
 
-function onRequest(request, response) { // node.js가 알아서 넣어줄 것임
-    response.writeHead(200, {'Content-Type' : 'text/html'});
-    response.write('Hello Node.js');
-    response.end();
+function start() {
+    // 서버를 생성하고 실행시키는 부분 
+    function onRequest(request, response) { // node.js가 알아서 넣어줄 것임
+        response.writeHead(200, {'Content-Type' : 'text/html'});
+        response.write('Hello Node.js');
+        response.end();
+    }
+
+    http.createServer(onRequest).listen(8888); // localhost:8888
 }
 
-http.createServer(onRequest).listen(8888); // localhost:8888
+exports.start = start;
