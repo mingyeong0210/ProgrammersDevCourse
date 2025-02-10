05e76f3ea8968e02c9c7843ffcd85b6492b0f1c5
mingyeong0210 <mica7150@naver.com>
2025-02-10 16:35:40 +0900

    feat: is-odd-num 실습

---
+++ 4주차/NodeBase/is-odd-demo.js
@@ -0,0 +1,8 @@
+// npm i is-odd-num
+const isOdd = require('is-odd-number');
+
+console.log(isOdd('1')); //=> true
+console.log(isOdd('3')); //=> true
+
+console.log(isOdd(0)); //=> false
+console.log(isOdd(2)); //=> false
