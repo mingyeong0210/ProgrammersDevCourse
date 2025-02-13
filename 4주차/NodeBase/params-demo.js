1d4a3394a8940cf2951f95b6a74c0e3a675f7959
mingyeong0210 <mica7150@naver.com>
2025-02-13 14:38:30 +0900

    feat: 쿼리스트링 실습

--- 4주차/NodeBase/params-demo.js
+++ 4주차/NodeBase/params-demo.js
@@ -6,8 +6,40 @@ app.get('/products/:n', function(req, res) {
     // console.log(req.params)
     // console.log(req.params.n)
 
+    // 자바스크립트는 문자로 된 숫자도 알아서 숫자로 인식하고 처리해줌 
+    if ((req.params.n - 10) > 5) { 
+        console.log("url로 전달받은 숫자가 10보다 크네요.")
+    }
+
+    let number = parseInt(req.params.n) - 10 // 문자로 된 숫자를 숫자로 
+    console.log(number)
+
     res.json({
         num : req.params.n
+    })
+})
+
+
+// 유튜브 채널 주소 : https://www.youtube.com/@15ya_egg
+// app.get('/:nickname', function(req, res) {
+
+//     const param = req.params // 바뀔 일이 없기에 변수에 저장해놓고 사용
+
+//     res.json({
+//         channel : param.nickname
+//     })
+// })
+
+// 영상 주소 : https://www.youtube.com/watch?v=nwKyTzC7THI
+// 타임라인 주소 : https://www.youtube.com/watch?v=nwKyTzC7THI&t=290s
+app.get('/watch', function(req, res) {
+    const q = req.query
+    console.log(q.v)
+    console.log(q.t)
+
+    res.json({
+        video : q.v,
+        timeline :  q.t
     })
 })
 
