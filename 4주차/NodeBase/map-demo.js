8b5ba1e81a32068e7a0846a2c77f546e1424cf58
mingyeong0210 <mica7150@naver.com>
2025-02-14 14:17:56 +0900

    feat: 객체에 데이터 추가

--- 4주차/NodeBase/map-demo.js
+++ 4주차/NodeBase/map-demo.js
@@ -7,26 +7,26 @@ app.listen(3000)
 // localhost:3000/2 => Cup
 // localhost:3000/3 => Chair
 
-app.get('/:id', function(req, res) {
-    // const {id} = req.params // 문자로된 숫자를 숫자를 바꿀 것이기에 const대신 let
-    let {id} = req.params
-    console.log(id)
-    // console.log(db.get(id)) // id는 문자열인데 db에는 숫자로 들어가 있음 
+// app.get('/:id', function(req, res) {
+//     // const {id} = req.params // 문자로된 숫자를 숫자를 바꿀 것이기에 const대신 let
+//     let {id} = req.params
+//     console.log(id)
+//     // console.log(db.get(id)) // id는 문자열인데 db에는 숫자로 들어가 있음 
     
-    id = parseInt(id) //"숫자" => 숫자  
-    console.log(db.get(id)) 
+//     id = parseInt(id) //"숫자" => 숫자  
+//     console.log(db.get(id)) 
 
-    if (db.get(id) == undefined) { // 예외 처리리
-        res.json({
-            message : "없는 상품입니다."
-        })
-    } else {
-        res.json({
-            id : id,
-            productName : db.get(id)
-        })
-    }
-})
+//     if (db.get(id) == undefined) { // 예외 처리
+//         res.json({
+//             message : "없는 상품입니다."
+//         })
+//     } else {
+//         res.json({
+//             id : id,
+//             productName : db.get(id)
+//         })
+//     }
+// })
 
 let db = new Map()
 // String만 담은 Map
@@ -72,3 +72,20 @@ console.log(db.get(1)) // NoteBook
 console.log(db.get(2)) // Cup
 console.log(db.get(3)) // Chair
 console.log(db.get(4)) // Poster 
+
+app.get('/:id', function(req, res) {
+    let {id} = req.params    
+    id = parseInt(id)
+
+    if (db.get(id) == undefined) { 
+        res.json({
+            message : "없는 상품입니다."
+        })
+    } else {
+        // 자바스크립트 객체에 데이터 추가
+        product = db.get(id)
+        product["id"] = id // product.id = id 
+
+        res.json(product) // id도 같이 보내고 싶다면?? 
+    }
+})
