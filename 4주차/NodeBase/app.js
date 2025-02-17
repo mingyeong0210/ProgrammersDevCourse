b17ad2285a0eac1c95e050d68ce91b272295382e
mingyeong0210 <mica7150@naver.com>
2025-02-17 16:05:00 +0900

    feat: post api 실습

--- 4주차/NodeBase/app.js
+++ 4주차/NodeBase/app.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 1234
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.use(express.json()) // express의 json이라는 미들웨어 사용하는 방법
+app.post('/test', (req, res) => {
+  // body에 숨겨져서 들어온 데이터를 화면에 뿌려줘볼까?
+  console.log(req.body.message)
+  res.json(req.body)
 })
 
 app.listen(port, () => {
