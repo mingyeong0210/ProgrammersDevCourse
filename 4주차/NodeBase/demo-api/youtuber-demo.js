a4648acef9dd87f7a14b29f0dd9394969914e685
mingyeong0210 <mica7150@naver.com>
2025-02-18 20:41:51 +0900

    feat: 전체 삭제 API

--- 4주차/NodeBase/demo-api/youtuber-demo.js
+++ 4주차/NodeBase/demo-api/youtuber-demo.js
@@ -89,3 +89,20 @@ app.delete('/youtubers/:id', (req, res) => {
         })
     }
 })
+
+// 전체 삭제
+app.delete('/youtubers', (req, res) => {
+    var msg = ""
+
+    // db에 값이 1개 이상이면, 전체 삭제
+    if (db.size >= 1) {
+        db.clear()
+        msg = "전체 유튜버가 삭제되었습니다."
+    } else {  // 값이 없으면,
+        msg = "삭제할 유튜버가 없습니다."
+    }
+
+    res.json({
+        message : msg
+    })
+})
