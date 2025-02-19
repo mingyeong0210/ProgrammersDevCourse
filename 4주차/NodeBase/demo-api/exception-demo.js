caa6432217468c16520e9c8f941a55b9e627b763
mingyeong0210 <mica7150@naver.com>
2025-02-19 17:01:34 +0900

    feat: json array, find()

---
+++ 4주차/NodeBase/demo-api/exception-demo.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+app.listen(1234)
+
+// 배열(리스트)
+const fruits = [
+    { id : 1, name : 'apple'},
+    { id : 2, name : 'orange'},
+    { id : 3, name : 'strawberry'},
+    { id : 4, name : 'blueberry'}
+] 
+
+// 과일 전체 조회
+app.get('/fruits', (req, res) => {
+    res.json(fruits) // json array
+})
+
+// 과일 개별 조회
+app.get('/fruits/:id', (req, res) => {
+    let id = req.params.id
+    // 배열이기 때문에 인덱스가 0부터 시작
+
+    // 방법1. id값에 1 빼기
+    // let fruit = fruits[id - 1] 
+
+    // 방법2. 객체 중에 id가 일치하는 값 찾기
+    // var findFruit = ""
+
+    // fruits.forEach(function(fruit) {
+    //     if (fruit.id == id) {
+    //         findFruit = fruit
+    //     }
+    // })
+
+    // 위 코드를 한줄로 줄이면
+    var findFruit = fruits.find(f => f.id == id) // fruits 배열 안에 있는 객체 중, id 값이 params.id랑 같은 객체를 찾겠다
+    
+
+    res.json(findFruit)
+})
