d5b07e62750d3bf3a2321dcb0cfb65958fd8fe9c
mingyeong0210 <mica7150@naver.com>
2025-02-20 16:39:51 +0900

    feat: 채널 개별 삭제

--- 5주차/YoutubeDemo/channel-demo.js
+++ 5주차/YoutubeDemo/channel-demo.js
@@ -44,5 +44,19 @@ app
         res.send("개별 수정")
     })
     .delete((req, res) => { // 채널 개별 삭제
-        res.send("개별 삭제")
+        let {id} = req.params
+        id = parseInt(id)
+
+        var channel = db.get(id)
+        if (channel) {
+            db.delete(id)
+            
+            res.status(200).json({
+                message : `${channel.channelTitle}이 정상적으로 삭제되었습니다.`
+            })
+        } else {
+            res.status(404).json({
+                message : `채널 정보를 찾을 수 없습니다.`
+            })
+        }
     })
