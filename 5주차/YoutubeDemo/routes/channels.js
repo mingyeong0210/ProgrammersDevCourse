45af62c606d465065480488498ee729224845545
mingyeong0210 <mica7150@naver.com>
2025-02-21 22:51:09 +0900

    feat: 채널 전체 조회 예외 처리

--- 5주차/YoutubeDemo/routes/channels.js
+++ 5주차/YoutubeDemo/routes/channels.js
@@ -8,15 +8,32 @@ let id = 1
 
 router
     .route('/')
-    .get((req, res) => { // 채널 전체 조회
+    .get((req, res) => { // (회원의) 채널 전체 조회
         if (db.size) {
+            var {userId} = req.body
             var channels = []
-        
-            db.forEach(function(value, key) {
-                channels.push(value)
-            })
-    
-            res.status(200).json(channels)
+            
+            // if (!userId)로도 가능! 
+            if (!userId) { // 예외 처리 1. userId가 body에 없을 때
+                res.status(404).json({
+                    message : `로그인이 필요한 페이지입니다.`
+                })
+            } else {
+                db.forEach(function(value, key) {
+                    if (value.userId === userId) {
+                        channels.push(value)
+                    }
+                })
+
+                // 예외 처리 2. userId가 가진 채널이 없을 때
+                if (channels.length == 0) {
+                    res.status(404).json({
+                        message : `조회할 채널이 없습니다.`
+                    })
+                } else {
+                    res.status(200).json(channels)
+                }
+            }
         } else {
             res.status(404).json({
                 message : `조회할 채널이 없습니다.`
@@ -25,7 +42,8 @@ router
     }) 
     .post((req, res) => { // 채널 개별 생성
         if (req.body.channelTitle) {
-            db.set(id++, req.body)
+            let channel = req.body
+            db.set(id++, channel)
 
             res.status(201).json({
                 message : `${db.get(id-1).channelTitle} 채널을 응원합니다!`
