6b868219058211d37905ee5c83a72af115d45778
mingyeong0210 <mica7150@naver.com>
2025-02-26 17:58:07 +0900

    feat: timezone세팅

--- 6주차/YoutubeDemo/db-demo.js
+++ 6주차/YoutubeDemo/db-demo.js
@@ -6,14 +6,21 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
-    database: 'Youtube'
+    database: 'Youtube',
+    dateStrings : true
 });
 
 // simple query
 connection.query(
     'SELECT * FROM `users`',
     function(err, results, fields) {
-        console.log(results); // results contains rows returned by server // 행 내용을 json array형태로 출력
-        console.log(fields); // fields contains extra meta data about results // results 외에 테이블이 가진 정보에 대한 내용
+        // results : 행 내용을 json array형태로 출력
+        // fields : results 외에 테이블이 가진 정보에 대한 내용
+
+        var {id, email, name, created_at} = results[0];
+        console.log(id); 
+        console.log(email); 
+        console.log(name); 
+        console.log(created_at);
     }
 );
