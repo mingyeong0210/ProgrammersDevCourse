c3bb00b0c05cbd0cce98c3bcc9867f7ea212e992
mingyeong0210 <mica7150@naver.com>
2025-03-12 21:40:41 +0900

    feat: Bookshop 비밀번호 초기화 api

--- 8주차/BookShop/controller/userController.js
+++ 8주차/BookShop/controller/userController.js
@@ -58,11 +58,47 @@ const login = (req, res) => {
 };
 
 const passwordResetRequest = (req, res) => {
+    const {email} = req.body;
 
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email, 
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            // 이메일로 유저가 있는지 찾아봄
+            const user =  results[0];
+            if(user) {
+                return res.status(StatusCodes.OK).json({
+                    email : email
+                });
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+        }
+    )
 };
 
 const passwordReset = (req, res) => {
+    const {email, password} = req.body;
 
+    let sql = 'UPDATE users SET password=? WHERE email=?';
+    let values = [password, email];
+    conn.query(sql, values, 
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            if(results.affectedRows == 0) 
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            else
+                return res.status(StatusCodes.OK).end();
+        }
+    )
 };
 
 module.exports = {
