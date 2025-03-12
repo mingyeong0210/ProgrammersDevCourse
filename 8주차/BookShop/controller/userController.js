95c8844200a32307978b034980b05aff56f7b327
mingyeong0210 <mica7150@naver.com>
2025-03-12 21:21:19 +0900

    feat: Bookshop 로그인 api

--- 8주차/BookShop/controller/userController.js
+++ 8주차/BookShop/controller/userController.js
@@ -1,5 +1,8 @@
 const conn = require('../mariadb');
 const { StatusCodes } = require('http-status-codes');
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const join = (req, res) => {
     const {email, password} = req.body;
@@ -16,6 +19,55 @@ const join = (req, res) => {
             return res.status(StatusCodes.CREATED).json(results);
         }
     )
-}
+};
 
-module.exports = join;
+const login = (req, res) => {
+    const {email, password} = req.body;
+
+    let sql = 'SELECT * FROM users WHERE email = ?';
+    conn.query(sql, email, 
+        (err, results) => {
+            if (err) {
+                console.log(err);
+                return res.status(StatusCodes.BAD_REQUEST).end();
+            }
+
+            const loginUser = results[0];
+
+            if(loginUser && loginUser.password == password) {
+                // 토큰 발행
+                const token = jwt.sign({
+                    email : loginUser.email,
+                }, process.env.PRIVATE_KEY, {
+                    expiresIn : '5m',
+                    issuer : 'mingyeong'
+                });
+
+                // 토큰 쿠키에 담기 
+                res.cookie("token", token, {
+                    httpOnly : true
+                });
+                console.log(token);
+
+                return res.status(StatusCodes.OK).json(results);
+            } else {
+                return res.status(StatusCodes.UNAUTHORIZED).end();
+            }
+        }
+    )
+};
+
+const passwordResetRequest = (req, res) => {
+
+};
+
+const passwordReset = (req, res) => {
+
+};
+
+module.exports = {
+    join, 
+    login, 
+    passwordResetRequest, 
+    passwordReset
+};
