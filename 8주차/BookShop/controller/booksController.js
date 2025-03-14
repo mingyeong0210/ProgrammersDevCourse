69125dbf70b3a7cfbbfe1f09b2bdd3059fee972c
mingyeong0210 <mica7150@naver.com>
2025-03-14 18:02:08 +0900

    feat: 도서 조회 페이징 적용

--- 8주차/BookShop/controller/booksController.js
+++ 8주차/BookShop/controller/booksController.js
@@ -3,22 +3,28 @@ const { StatusCodes } = require('http-status-codes');
 
 // (카테고리별, 신간 여부) 전체 도서 목록 조회
 const allBooks = (req, res) => {
-    let {category_id, news} = req.query;
+    let {category_id, news, limit, currentPage} = req.query;
+
+    // limit : page 당 도서 수 
+    // currentPage : 현재 페이지
+    let offset = limit * (currentPage-1);
 
     let sql = 'SELECT * FROM books ';
     let values = [];
     if(category_id && news) {
         sql += 'WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = [category_id, news];
+        values = [category_id];
     }
     else if(category_id) {
         sql += 'WHERE category_id=?';
-        values = category_id;
+        values = [category_id];
     }
     else if(news) {
         sql += 'WHERE pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
-        values = news;
     }
+
+    sql += ' LIMIT ? OFFSET ?';
+    values.push(parseInt(limit), offset);
 
     conn.query(sql, values,
         (err, results) => {
