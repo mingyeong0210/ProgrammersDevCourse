81055ae0a335b243f3e6834c9bc584850cb3b3b4
mingyeong0210 <mica7150@naver.com>
2025-03-15 21:18:18 +0900

    feat: 도서 조회 시 좋아요 관련 내용 추가

--- 8주차/BookShop/controller/booksController.js
+++ 8주차/BookShop/controller/booksController.js
@@ -9,7 +9,7 @@ const allBooks = (req, res) => {
     // currentPage : 현재 페이지
     let offset = limit * (currentPage-1);
 
-    let sql = 'SELECT * FROM books ';
+    let sql = 'SELECT *, (SELECT count(*) FROM likes WHERE liked_book_id=books.id) AS likes FROM books ';
     let values = [];
     if(category_id && news) {
         sql += 'WHERE category_id=? AND pub_date BETWEEN DATE_SUB(NOW(), INTERVAL 1 MONTH) AND NOW()';
@@ -42,12 +42,20 @@ const allBooks = (req, res) => {
 };
 
 const bookDetail = (req, res) => {
-    const {id} = req.params;
+    let book_id = req.params.id;
+    let {user_id} = req.body;
 
-    let sql = `SELECT * FROM books LEFT JOIN category 
-                ON books.category_id = category.id WHERE books.id=?`;
+    let sql = `SELECT *,
+                    (SELECT count(*) FROM likes WHERE liked_book_id = books.id) AS likes,
+                    (SELECT EXISTS (SELECT * FROM likes WHERE user_id = ? AND liked_book_id = ?)) AS liked
+                FROM books 
+                LEFT JOIN category 
+                ON books.category_id = category.category_id
+                WHERE books.id = ?;`;
+
+    let values = [user_id, book_id, book_id];
     
-    conn.query(sql, id,
+    conn.query(sql, values,
         (err, results) => {
             if (err) {
                 console.log(err);
