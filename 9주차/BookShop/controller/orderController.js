3de35ffcfe1b50951838815544e71284faf45691
mingyeong0210 <mica7150@naver.com>
2025-03-20 22:56:32 +0900

    feat: 주문 상세 조회 API

--- 9주차/BookShop/controller/orderController.js
+++ 9주차/BookShop/controller/orderController.js
@@ -62,15 +62,32 @@ const getOrders = async (req, res) => {
 
     let sql = `SELECT orders.id, created_at, address, receiver, contact, book_title, total_quantity, total_price
                 FROM orders LEFT JOIN delivery
-                ON orders.delivery_id = delivery.id;`
+                ON orders.delivery_id = delivery.id`;
 
     let [rows, fields] = await conn.query(sql);
 
     return res.status(StatusCodes.OK).json(rows);
 };
 
-const getOrderDetail = (req, res) => {
+const getOrderDetail = async (req, res) => {
+    const {id} = req.params;
 
+    const conn = await mariadb.createConnection({
+        host : 'localhost',
+        user : 'root',
+        password : 'root',
+        database : 'Bookshop',
+        dateStrings : true
+    });
+
+    let sql = `SELECT book_id, title, author, price, quantity
+                FROM orderedBook LEFT JOIN books
+                ON orderedBook.book_id = books.id
+                WHERE order_id = ?`;
+
+    let [rows, fields] = await conn.query(sql, [id]);
+
+    return res.status(StatusCodes.OK).json(rows);
 };
 
 module.exports =  {
