3b3fc8889050f4767e869744405ffdda351fb614
mingyeong0210 <mica7150@naver.com>
2025-03-24 00:17:26 +0900

    feat: try/catch, throw 실습

--- 9주차/NodeBase/demo-js/try-catch-demo.js
+++ 9주차/NodeBase/demo-js/try-catch-demo.js
@@ -1,9 +1,16 @@
-let string = '{ "num1" :  1';
+let string = '{ "num1" :  1}';
 
 try {
-    // username;
     let json = JSON.parse(string);
-    console.log(json);
+
+    if(!json.name) {
+        throw new SyntaxError("입력 값에 이름이 없습니다.");
+    } else {
+        console.log(json.name);
+    }
+
+    let name = json.name;
+    console.log(name);
 } catch(err) {
     console.log(err.name);
     console.log(err.message);
