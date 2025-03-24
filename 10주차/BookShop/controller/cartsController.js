7cb6faf3c506e7dba9a9b102a452102a802c3a9b
mingyeong0210 <mica7150@naver.com>
2025-03-24 15:19:07 +0900

    feat: 유저의 장바구니 목록 전체 조회 수정

--- 10주차/BookShop/controller/cartsController.js
+++ 10주차/BookShop/controller/cartsController.js
@@ -55,8 +55,13 @@ const getCartItems = (req, res) => {
         let sql = `SELECT cartItems.id, book_id, title, summary, quantity, price 
                     FROM cartItems LEFT JOIN books 
                     ON cartItems.book_id = books.id
-                    WHERE user_id = ? AND cartItems.id IN (?)`;
-        let values = [authorization.id, selected];
+                    WHERE user_id = ? `;
+        let values = [authorization.id];
+
+        if(selected) { // 주문서 작성 시 '선택한 장바구니 목록 조회'
+            sql += 'AND cartItems.id IN (?)';
+            values.push(selected);
+        };
     
         conn.query(sql, values,
             (err, results) => {
@@ -69,7 +74,6 @@ const getCartItems = (req, res) => {
             }
         );
     }
-
 };
 
 // 장바구니 아이템 개별 삭제
