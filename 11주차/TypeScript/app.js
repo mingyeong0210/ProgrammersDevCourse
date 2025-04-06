d54efeb68af615dffec6fcfdeb0441c81c83c745
mingyeong0210 <mica7150@naver.com>
2025-04-06 17:21:43 +0900

    feat: 타입 명시

--- 11주차/TypeScript/app.js
+++ 11주차/TypeScript/app.js
@@ -1,4 +1,28 @@
-function logName(name) {
-    console.log(name);
+// function logName(name : string) {
+//     console.log(name);
+// }
+// logName('kmg');
+// let student = {
+//     name : 'john',
+//     course : 'typescript',
+//     score : 100,
+//     grade : function(){
+//         console.log('A');
+//     }
+// }
+// student.name = 'lee';
+// student.score = 200;
+// 변수의 데이터 타입 명시
+var stdId = 1111;
+var stdName = 'lee';
+var age = 20;
+var gender = 'male';
+var course = 'Typescript';
+var completed = false;
+function getInfo(id) {
+    return null;
 }
-logName('kmg');
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+function Plus(a, b) {
+    return a + b;
+}
--- 11주차/TypeScript/app.ts
+++ 11주차/TypeScript/app.ts
@@ -1,5 +1,42 @@
-function logName(name : string) {
-    console.log(name);
+// function logName(name : string) {
+//     console.log(name);
+// }
+
+// logName('kmg');
+
+// let student = {
+//     name : 'john',
+//     course : 'typescript',
+//     score : 100,
+//     grade : function(){
+//         console.log('A');
+//     }
+// }
+
+// student.name = 'lee';
+// student.score = 200;
+
+// 변수의 데이터 타입 명시
+let stdId : number = 1111;
+let stdName : string = 'lee';
+let age : number = 20;
+let gender : string = 'male';
+let course : string = 'Typescript';
+let completed : boolean= false;
+
+function getInfo(id : number) : {
+    stdId : number;
+    stdName : string;
+    age : number;
+    gender : string;
+    course : string;
+    completed : boolean;
+} 
+{
+    return null;
 }
 
-logName('kmg');
+// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+function Plus(a : number, b : number) : number {
+    return a + b;
+}
