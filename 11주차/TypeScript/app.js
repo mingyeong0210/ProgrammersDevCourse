5dfb810123010c5051b17768ff8832720bf6a863
mingyeong0210 <mica7150@naver.com>
2025-04-06 23:40:01 +0900

    feat: union, type alias, typeof 실습

--- 11주차/TypeScript/app.js
+++ 11주차/TypeScript/app.js
@@ -1,72 +1,19 @@
 // function logName(name : string) {
 //     console.log(name);
 // }
-// logName('kmg');
-// let student = {
-//     name : 'john',
-//     course : 'typescript',
-//     score : 100,
-//     grade : function(){
-//         console.log('A');
-//     }
-// }
-// student.name = 'lee';
-// student.score = 200;
-// 변수의 데이터 타입 명시
-var stdId = 1111;
-var stdName = 'lee';
-var age = 20;
-var gender = 'male';
-var course = 'Typescript';
-var completed = false;
-// 열거형 : 사용자 정의 타입
-var GenderType;
-(function (GenderType) {
-    GenderType[GenderType["Male"] = 0] = "Male";
-    GenderType[GenderType["Female"] = 1] = "Female";
-})(GenderType || (GenderType = {}));
-var MyStudent = /** @class */ (function () {
-    function MyStudent() {
-        this.stdId = 91011;
-        this.stdName = 'park';
-        this.age = 30;
-        this.gender = GenderType.Male;
-        this.course = 'node.js';
-        this.completed = true;
+var numStr = '100';
+var item;
+function convertToString(val) {
+    if (typeof val == 'string') {
+        item = 0;
     }
-    MyStudent.prototype.setName = function (name) {
-        this.stdName = name;
-        console.log('이름 설정 : ' + this.stdName);
-    };
-    return MyStudent;
-}());
-var myInstance = new MyStudent();
-myInstance.setName('앨리스');
-// function getInfo(id : number) : Student
-// {
-//     return {
-//         stdId : id,
-//         stdName : 'lee',
-//         // age : 20,
-//         gender : GenderType.Female,
-//         course : 'javascript',
-//         completed : true,
-//     };
-// }
-// let std = { // new 생략되어있는 것. 힙 메모리에 할당 
-//     stdId : 91011,
-//     stdName : 'park',
-//     age : 30,
-//     gender : GenderType.Male,
-//     course : 'node.js',
-//     completed : true,
-// }
-// function setInfo(student : Student) : void {
-//     console.log(student);
-// }
-// setInfo(std);
-// console.log(getInfo(5678));
-// // 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-// function Plus(a : number, b : number) : number {
-//     return a + b;
-// }
+    else {
+        item = val;
+    }
+    return String(val);
+}
+function convertToNumber(val) {
+    return Number(val);
+}
+console.log(convertToString(numStr));
+console.log(convertToNumber(numStr));
--- 11주차/TypeScript/app.ts
+++ 11주차/TypeScript/app.ts
@@ -16,48 +16,48 @@
 // student.name = 'lee';
 // student.score = 200;
 
-// 변수의 데이터 타입 명시
-let stdId : number = 1111;
-let stdName : string = 'lee';
-let age : number = 20;
-let gender : string = 'male';
-let course : string = 'Typescript';
-let completed : boolean = false;
+// // 변수의 데이터 타입 명시
+// let stdId : number = 1111;
+// let stdName : string = 'lee';
+// let age : number = 20;
+// let gender : string = 'male';
+// let course : string = 'Typescript';
+// let completed : boolean = false;
 
-// 열거형 : 사용자 정의 타입
-enum GenderType {
-    Male,
-    Female
-}
+// // 열거형 : 사용자 정의 타입
+// enum GenderType {
+//     Male,
+//     Female
+// }
 
-interface Student {
-    stdId : number;
-    stdName? : string;
-    age? : number;
-    gender? : GenderType;
-    course? : string;
-    completed? : boolean;
-    // setName(name : string) : void; 
-    setName : (name : string) => void; // 함수를 하나의 프로퍼티로 사용 
-    // getName : () => string;
-}
+// interface Student {
+//     stdId : number;
+//     stdName? : string;
+//     age? : number;
+//     gender? : 'male' | 'female';
+//     course? : string;
+//     completed? : boolean;
+//     // setName(name : string) : void; 
+//     setName : (name : string) => void; // 함수를 하나의 프로퍼티로 사용 
+//     // getName : () => string;
+// }
 
-class MyStudent implements Student{
-    stdId = 91011;
-    stdName = 'park';
-    age = 30;
-    gender = GenderType.Male;
-    course = 'node.js';
-    completed = true;
+// class MyStudent implements Student{
+//     stdId = 91011;
+//     stdName = 'park';
+//     age = 30;
+//     gender : 'male' | 'female' = 'male';
+//     course = 'node.js';
+//     completed = true;
 
-    setName(name : string) : void { // 오버라이딩 
-        this.stdName = name;
-        console.log('이름 설정 : ' + this.stdName);
-    }
-}
+//     setName(name : string) : void { // 오버라이딩 
+//         this.stdName = name;
+//         console.log('이름 설정 : ' + this.stdName);
+//     }
+// }
 
-const myInstance = new MyStudent();
-myInstance.setName('앨리스');
+// const myInstance = new MyStudent();
+// myInstance.setName('앨리스');
 
 // function getInfo(id : number) : Student
 // {
@@ -65,7 +65,7 @@ myInstance.setName('앨리스');
 //         stdId : id,
 //         stdName : 'lee',
 //         // age : 20,
-//         gender : GenderType.Female,
+//         gender : 'female',
 //         course : 'javascript',
 //         completed : true,
 //     };
@@ -75,7 +75,7 @@ myInstance.setName('앨리스');
 //     stdId : 91011,
 //     stdName : 'park',
 //     age : 30,
-//     gender : GenderType.Male,
+//     gender : 'male',
 //     course : 'node.js',
 //     completed : true,
 // }
@@ -92,3 +92,39 @@ myInstance.setName('앨리스');
 // function Plus(a : number, b : number) : number {
 //     return a + b;
 // }
+
+// // 객체 타입 명시 
+// const user : {name : string, age : number} = {
+//     name : 'john',
+//     age : 25
+// };
+
+// any 
+// let anyVal : any = 100;
+// anyVal = 'kim';
+
+// union
+// let numStr : number | string = '100';
+
+// 타입별칭 
+type strOrNum = number | string;
+
+let numStr : strOrNum = '100';
+let item : number;
+
+function convertToString(val : strOrNum) : string {
+    if(typeof val == 'string') {
+        item = 0;
+    } else {
+        item = val;
+    }
+
+    return String(val);
+}
+
+function convertToNumber(val : strOrNum) : number {
+    return Number(val);
+}
+
+console.log(convertToString(numStr));
+console.log(convertToNumber(numStr));
