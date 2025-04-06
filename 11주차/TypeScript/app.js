2604f106fabbe72a504ab13a40f5928680cee891
mingyeong0210 <mica7150@naver.com>
2025-04-06 23:03:13 +0900

    feat: enum 실습

--- 11주차/TypeScript/app.js
+++ 11주차/TypeScript/app.js
@@ -19,10 +19,54 @@ var age = 20;
 var gender = 'male';
 var course = 'Typescript';
 var completed = false;
-function getInfo(id) {
-    return null;
-}
-// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-function Plus(a, b) {
-    return a + b;
-}
+// 열거형 : 사용자 정의 타입
+var GenderType;
+(function (GenderType) {
+    GenderType[GenderType["Male"] = 0] = "Male";
+    GenderType[GenderType["Female"] = 1] = "Female";
+})(GenderType || (GenderType = {}));
+var MyStudent = /** @class */ (function () {
+    function MyStudent() {
+        this.stdId = 91011;
+        this.stdName = 'park';
+        this.age = 30;
+        this.gender = GenderType.Male;
+        this.course = 'node.js';
+        this.completed = true;
+    }
+    MyStudent.prototype.setName = function (name) {
+        this.stdName = name;
+        console.log('이름 설정 : ' + this.stdName);
+    };
+    return MyStudent;
+}());
+var myInstance = new MyStudent();
+myInstance.setName('앨리스');
+// function getInfo(id : number) : Student
+// {
+//     return {
+//         stdId : id,
+//         stdName : 'lee',
+//         // age : 20,
+//         gender : GenderType.Female,
+//         course : 'javascript',
+//         completed : true,
+//     };
+// }
+// let std = { // new 생략되어있는 것. 힙 메모리에 할당 
+//     stdId : 91011,
+//     stdName : 'park',
+//     age : 30,
+//     gender : GenderType.Male,
+//     course : 'node.js',
+//     completed : true,
+// }
+// function setInfo(student : Student) : void {
+//     console.log(student);
+// }
+// setInfo(std);
+// console.log(getInfo(5678));
+// // 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number {
+//     return a + b;
+// }
--- 11주차/TypeScript/app.ts
+++ 11주차/TypeScript/app.ts
@@ -22,21 +22,73 @@ let stdName : string = 'lee';
 let age : number = 20;
 let gender : string = 'male';
 let course : string = 'Typescript';
-let completed : boolean= false;
+let completed : boolean = false;
 
-function getInfo(id : number) : {
+// 열거형 : 사용자 정의 타입
+enum GenderType {
+    Male,
+    Female
+}
+
+interface Student {
     stdId : number;
-    stdName : string;
-    age : number;
-    gender : string;
-    course : string;
-    completed : boolean;
-} 
-{
-    return null;
+    stdName? : string;
+    age? : number;
+    gender? : GenderType;
+    course? : string;
+    completed? : boolean;
+    // setName(name : string) : void; 
+    setName : (name : string) => void; // 함수를 하나의 프로퍼티로 사용 
+    // getName : () => string;
 }
 
-// 함수의 데이터 타입 명시 (매개변수, 리턴타입)
-function Plus(a : number, b : number) : number {
-    return a + b;
+class MyStudent implements Student{
+    stdId = 91011;
+    stdName = 'park';
+    age = 30;
+    gender = GenderType.Male;
+    course = 'node.js';
+    completed = true;
+
+    setName(name : string) : void { // 오버라이딩 
+        this.stdName = name;
+        console.log('이름 설정 : ' + this.stdName);
+    }
 }
+
+const myInstance = new MyStudent();
+myInstance.setName('앨리스');
+
+// function getInfo(id : number) : Student
+// {
+//     return {
+//         stdId : id,
+//         stdName : 'lee',
+//         // age : 20,
+//         gender : GenderType.Female,
+//         course : 'javascript',
+//         completed : true,
+//     };
+// }
+
+// let std = { // new 생략되어있는 것. 힙 메모리에 할당 
+//     stdId : 91011,
+//     stdName : 'park',
+//     age : 30,
+//     gender : GenderType.Male,
+//     course : 'node.js',
+//     completed : true,
+// }
+
+// function setInfo(student : Student) : void {
+//     console.log(student);
+// }
+
+// setInfo(std);
+
+// console.log(getInfo(5678));
+
+// // 함수의 데이터 타입 명시 (매개변수, 리턴타입)
+// function Plus(a : number, b : number) : number {
+//     return a + b;
+// }
