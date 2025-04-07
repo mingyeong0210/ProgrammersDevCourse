c18ff7a879db60a55375e10f43b4386a0cfb7f59
mingyeong0210 <mica7150@naver.com>
2025-04-07 18:08:36 +0900

    feat: 생성자, 접근지정자, get, set 실습

--- 11주차/TypeScript/oop.js
+++ 11주차/TypeScript/oop.js
@@ -6,17 +6,40 @@
 //     console.log(`${empName}의 나이는 ${age}이고, 직업은 ${empJob}입니다.`);
 // }
 var Employee = /** @class */ (function () {
-    function Employee() {
+    // // 멤버 변수 == 속성 == 프로퍼티 : 클래스 안에서 생성된 변수
+    // private _empName : string;
+    // private _age : number;
+    // private _empJob : string;
+    // constructor(empName : string, age? : number, empJob? : string) { // 생성자
+    //     this._empName = empName;
+    //     this._age = age;
+    //     this._empJob = empJob;
+    // }; 
+    // 위 코드를 간략하게 표현한 코드
+    function Employee(// 매개변수들이 암묵적으로 클래스의 멤버로 선언됨과 동시에 전달인자로 사용됨
+    _empName, _age, _empJob) {
         var _this = this;
+        this._empName = _empName;
+        this._age = _age;
+        this._empJob = _empJob;
         // 멤버 함수 == 메소드 
         this.printEmp = function () {
-            console.log("".concat(_this.empName, "\uC758 \uB098\uC774\uB294 ").concat(_this.age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this.empJob, "\uC785\uB2C8\uB2E4."));
+            console.log("".concat(_this._empName, "\uC758 \uB098\uC774\uB294 ").concat(_this._age, "\uC774\uACE0, \uC9C1\uC5C5\uC740 ").concat(_this._empJob, "\uC785\uB2C8\uB2E4."));
         };
     }
+    Object.defineProperty(Employee.prototype, "empName", {
+        // get set 
+        get: function () {
+            return this._empName;
+        },
+        set: function (val) {
+            this._empName = val;
+        },
+        enumerable: false,
+        configurable: true
+    });
     return Employee;
 }());
-var employee1 = new Employee(); // 객체 생성
-employee1.empName = 'kim';
-employee1.age = 20;
-employee1.empJob = '개발자';
+var employee1 = new Employee('kim', 30, '소프트웨어개발자'); // 객체 생성
+employee1.empName = 'lee';
 employee1.printEmp();
--- 11주차/TypeScript/oop.ts
+++ 11주차/TypeScript/oop.ts
@@ -8,20 +8,41 @@
 // }
 
 class Employee {
-    // 멤버 변수 == 속성 == 프로퍼티 : 클래스 안에서 생성된 변수
-    empName : string;
-    age : number;
-    empJob : string;
+    // // 멤버 변수 == 속성 == 프로퍼티 : 클래스 안에서 생성된 변수
+    // private _empName : string;
+    // private _age : number;
+    // private _empJob : string;
     
+    // constructor(empName : string, age? : number, empJob? : string) { // 생성자
+    //     this._empName = empName;
+    //     this._age = age;
+    //     this._empJob = empJob;
+    // }; 
+
+    // 위 코드를 간략하게 표현한 코드
+    constructor( // 매개변수들이 암묵적으로 클래스의 멤버로 선언됨과 동시에 전달인자로 사용됨
+        private _empName : string,
+        private _age : number,
+        private _empJob : string
+    ) {
+        
+    }
+
+    // get set 
+    get empName() {
+        return this._empName;
+    }
+
+    set empName(val : string) {
+        this._empName = val;
+    }
+
     // 멤버 함수 == 메소드 
     printEmp = () : void => {
-        console.log(`${this.empName}의 나이는 ${this.age}이고, 직업은 ${this.empJob}입니다.`);
+        console.log(`${this._empName}의 나이는 ${this._age}이고, 직업은 ${this._empJob}입니다.`);
     }
 }
 
-let employee1 = new Employee(); // 객체 생성
-employee1.empName = 'kim';
-employee1.age = 20;
-employee1.empJob = '개발자';
-
+let employee1 = new Employee('kim', 30, '소프트웨어개발자'); // 객체 생성
+employee1.empName = 'lee';
 employee1.printEmp();
