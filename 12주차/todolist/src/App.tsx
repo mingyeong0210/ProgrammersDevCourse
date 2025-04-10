8787cda5fac23bb43687b664d9ffe055d259a16c
mingyeong0210 <mica7150@naver.com>
2025-04-10 17:17:42 +0900

    feat: 시계 추가

--- 12주차/todolist/src/App.tsx
+++ 12주차/todolist/src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import TodoList from './Todolist';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Clock from './Timer';
 
 function App() {
   return (
     <div className="container">
       <TodoList></TodoList>
+      <Clock></Clock>
     </div>
   );
 }
