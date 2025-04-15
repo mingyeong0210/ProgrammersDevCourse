aed047e9fe960857088805e819cac69f4332a541
mingyeong0210 <mica7150@naver.com>
2025-04-16 03:08:51 +0900

    feat: task앱에 DragDrop기능 추가

--- src/components/ListContainer/ListContainer.tsx
+++ src/components/ListContainer/ListContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { FC } from 'react'
 import { IList } from '../../types';
 import List from '../List/List';
 import ActionButton from '../ActionButton/ActionButton';
