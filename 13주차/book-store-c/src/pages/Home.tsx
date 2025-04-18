8517eb6a26c00f931aa2818b474dac20fc3abd61
mingyeong0210 <mica7150@naver.com>
2025-04-18 23:29:43 +0900

    feat: Title 컴포넌트

--- 13주차/book-store-c/src/pages/Home.tsx
+++ 13주차/book-store-c/src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Title from "../components/common/Title";
+
 function Home() {
     return(
         <>
+        <Title size="large">제목 테스트</Title>
         <div>home body</div>
         </>
     )
