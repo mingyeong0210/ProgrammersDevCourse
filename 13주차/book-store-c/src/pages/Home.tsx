dbefc8696fe6b5aec6a72bc5157bfe0983cbf0bd
mingyeong0210 <mica7150@naver.com>
2025-04-18 23:56:01 +0900

    feat: Button 컴포넌트

--- 13주차/book-store-c/src/pages/Home.tsx
+++ 13주차/book-store-c/src/pages/Home.tsx
@@ -1,9 +1,13 @@
+import Button from "../components/common/Button";
 import Title from "../components/common/Title";
 
 function Home() {
     return(
         <>
-        <Title size="large">제목 테스트</Title>
+        <Title size="large" color="background">
+            제목 테스트
+        </Title>
+        <Button size="large" schema="primary">버튼 테스트</Button>
         <div>home body</div>
         </>
     )
--- 13주차/book-store-c/src/style/theme.ts
+++ 13주차/book-store-c/src/style/theme.ts
@@ -1,6 +1,10 @@
+import exp from "constants";
+
 export type ThemeName = `light` | `dark`;
 export type ColorKey = `primary` | `background` | `secondary` | `third`;
 export type HeadingSize = `large` | `medium` | `small`;
+export type ButtonSize = `large` | `medium` | `small`;
+export type ButtonSchema = `primary` | `normal`;
 
 interface Theme {
     name: ThemeName;
@@ -9,6 +13,21 @@ interface Theme {
         [key in HeadingSize]: {
             fontSize: string;
         };
+    };
+    button: {
+        [key in ButtonSize]: {
+            fontSize: string;
+            padding: string;
+        }
+    };
+    buttonSchema: {
+        [key in ButtonSchema]: {
+            color: string;
+            backgroundColor: string;
+        }
+    };
+    borderRadius: {
+        default: string;
     }
 }
 
@@ -30,7 +49,34 @@ export const light: Theme = {
         small: {
             fontSize: `1rem`,
         },
-    }    
+    },
+    button: {
+        large: {
+            fontSize: `1.5rem`,
+            padding: `1rem 2rem`,
+        },
+        medium: {
+            fontSize: `1rem`,
+            padding: `0.5rem 1rem`,
+        },
+        small: {
+            fontSize: `0.75rem`,
+            padding: `0.25rem 0.5rem`,
+        },
+    },
+    buttonSchema: {
+        primary: {
+            color: `white`,
+            backgroundColor: `midnightblue`,
+        },
+        normal: {
+            color: `black`,
+            backgroundColor: `lightgray`,
+        },
+    },
+    borderRadius: {
+        default: `4px`,
+    },
 };
 
 export const dark: Theme = {
