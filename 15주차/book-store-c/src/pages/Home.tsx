e51b4de93fa049e14b35a80d401ac2ad35857cf5
mingyeong0210 <mica7150@naver.com>
2025-05-02 16:59:13 +0900

    feat: 메인_배너 섹션

--- 15주차/book-store-c/src/pages/Home.tsx
+++ 15주차/book-store-c/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import Banner from "@/components/common/banner/Banner";
 import Title from "@/components/common/Title";
 import MainBest from "@/components/main/MainBest";
 import MainNewBooks from "@/components/main/MainNewBooks";
@@ -6,11 +7,12 @@ import { useMain } from "@/hooks/useMain";
 import styled from "styled-components";
 
 function Home() {
-    const { reviews, newBooks, bestBooks }  = useMain();
+    const { reviews, newBooks, bestBooks, banners }  = useMain();
 
     return(
         <HomeStyle>
             {/* 배너 */}
+            <Banner banners={banners} />
 
             {/* 베스트셀러 */}
             <section className="section">
